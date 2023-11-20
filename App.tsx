4e3623da06d803def8c158106619c144da70e155
Frederico <costafredericob@gmail.com>
2023-11-20 15:32:31 -0300

    base finalizada

--- App.tsx
+++ App.tsx
@@ -1,24 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import HelloView from './src/components/hello';
+import globalStyles from './src/styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.text}>Open up App.tsx to start working on your app!</Text>
       <HelloView />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff0969',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 16,
+  },
+});
+
+export default globalStyles;
